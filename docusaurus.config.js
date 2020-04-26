a123fa906891b22fb239f537c943a17841ea2e76
Hyeonki Hong <hhk7734@gmail.com>
2020-04-26 20:44:11 +0900

    sidebars.js: correct data format

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,83 +1,90 @@
-const path = require('path');
-const _fs = require('fs');
+const path = require("path");
+const _fs = require("fs");
 
 module.exports = {
-    title: "loliot",
-    tagline: "loliot",
-    url: "https://wiki.loliot.net",
-    baseUrl: "/",
-    favicon: "img/favicon.ico",
-    organizationName: "HandS",
-    projectName: "wiki.loliot.net",
-    plugins: [
-        "@docusaurus/plugin-google-analytics",
-        "@docusaurus/plugin-sitemap",
-        {
-            cacheTime: 600 * 1000, // 600 sec - cache purge period
-            changefreq: "weekly",
-            priority: 0.5,
-        },
-    ],
-    themeConfig: {
-        googleAnalytics: {
-            trackingID: "UA-82937088-4",
-            anonymizeIP: true,
-        },
-        navbar: {
-            title: "loliot",
-            logo: {
-                alt: "loliot Logo",
-                src: "img/logo.svg",
-            },
-            links: (() => {
-                const links = [];
-
-                let sidebarConfig = require("./sidebars");
-
-                _fs.readdirSync(path.join(path.dirname(__filename), "docs")).map((rootDir) => {
-                  const link = {
-                    label: rootDir,
-                    position: "left",
-                    items: []
-                  };
-
-                  _fs.readdirSync(path.join(path.dirname(__filename), "docs", rootDir)).map((subDir, subIndex) => {
-                    link.items.push({
-                      label: subDir,
-                      to: ""
-                    })
-                      if (subIndex === 0) link['to'] = `/docs/${sidebarConfig[subDir][Object.keys(sidebarConfig[subDir])[0]][0]}`
-                      link.items[subIndex]['to'] = `/docs/${sidebarConfig[subDir][Object.keys(sidebarConfig[subDir])[0]][0]}`
-                  });
-                  links.push(link)
-                })
-
-              links.push({
-                href: "https://github.com/hhk7734",
-                label: "GitHub",
-                position: "right",
-              })
-              return links
-            })(),
-        },
-        footer: {
-            style: "dark",
-            copyright: `Copyright © ${new Date().getFullYear()} HandS. Built with Docusaurus.`,
-        },
+  title: "loliot",
+  tagline: "loliot",
+  url: "https://wiki.loliot.net",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "HandS",
+  projectName: "wiki.loliot.net",
+  plugins: [
+    "@docusaurus/plugin-google-analytics",
+    "@docusaurus/plugin-sitemap",
+    {
+      cacheTime: 600 * 1000, // 600 sec - cache purge period
+      changefreq: "weekly",
+      priority: 0.5,
     },
-    presets: [
-        [
-            "@docusaurus/preset-classic",
-            {
-                defaultDarkMode: true,
-                docs: {
-                    sidebarPath: require.resolve("./sidebars.js"),
-                    showLastUpdateTime: true,
-                },
-                theme: {
-                    customCss: require.resolve("./src/css/custom.css"),
-                },
-            },
-        ],
+  ],
+  themeConfig: {
+    googleAnalytics: {
+      trackingID: "UA-82937088-4",
+      anonymizeIP: true,
+    },
+    navbar: {
+      title: "loliot",
+      logo: {
+        alt: "loliot Logo",
+        src: "img/logo.svg",
+      },
+      links: (() => {
+        const links = [];
+
+        const sidebarConfig = require("./sidebars");
+
+        _fs
+          .readdirSync(path.join(path.dirname(__filename), "docs"))
+          .map((rootDir) => {
+            const link = {
+              label: rootDir,
+              position: "left",
+              items: [],
+            };
+
+            _fs
+              .readdirSync(path.join(path.dirname(__filename), "docs", rootDir))
+              .map((subDir, subIndex) => {
+                link.items.push({
+                  label: subDir,
+                  to: "",
+                });
+                if (subIndex === 0)
+                  link["to"] = `/docs/${sidebarConfig[subDir][0]["items"][0]}`;
+                link.items[subIndex][
+                  "to"
+                ] = `/docs/${sidebarConfig[subDir][0]["items"][0]}`;
+              });
+            links.push(link);
+          });
+
+        links.push({
+          href: "https://github.com/hhk7734/wiki.loliot.net",
+          label: "GitHub",
+          position: "right",
+        });
+        return links;
+      })(),
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} HandS. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        defaultDarkMode: true,
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          showLastUpdateTime: true,
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
     ],
+  ],
 };
--- sidebars.js
+++ sidebars.js
@@ -1,11 +1,31 @@
 module.exports = {
-  cpp: {
-    Libraries: [
-      "lang/cpp/libraries/cpp-stl",
-      "lang/cpp/libraries/cpp-exception-handling",
-    ],
-  },
-  python: {
-    Libraries: ["lang/python/libraries/python-logging"],
-  },
+  cpp: [
+    {
+      type: "category",
+      label: "Libraries",
+      items: [
+        "lang/cpp/libraries/cpp-stl",
+        "lang/cpp/libraries/cpp-exception-handling",
+      ],
+    },
+    {
+      type: "category",
+      label: "Advanced C++",
+      items: [
+        "lang/cpp/advanced-cpp/cpp-constexpr",
+        {
+          type: "category",
+          label: "LLVM",
+          items: ["lang/cpp/advanced-cpp/llvm/llvm-basics"],
+        },
+      ],
+    },
+  ],
+  python: [
+    {
+      type: "category",
+      label: "Libraries",
+      items: ["lang/python/libraries/python-logging"],
+    },
+  ],
 };
