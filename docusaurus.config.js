33267698f01abf46dab305bf8c6ee6ae729bcc6e
HHongSeungWoo <qksn1541@gmail.com>
2020-04-26 18:36:21 +0900

    config.js: 마크다운 목록 자동화

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,72 +1,83 @@
+const path = require('path');
+const _fs = require('fs');
+
 module.exports = {
-  title: "loliot",
-  tagline: "loliot",
-  url: "https://wiki.loliot.net",
-  baseUrl: "/",
-  favicon: "img/favicon.ico",
-  organizationName: "HandS",
-  projectName: "wiki.loliot.net",
-  plugins: [
-    "@docusaurus/plugin-google-analytics",
-    "@docusaurus/plugin-sitemap",
-    {
-      cacheTime: 600 * 1000, // 600 sec - cache purge period
-      changefreq: "weekly",
-      priority: 0.5,
-    },
-  ],
-  themeConfig: {
-    googleAnalytics: {
-      trackingID: "UA-82937088-4",
-      anonymizeIP: true,
-    },
-    navbar: {
-      title: "loliot",
-      logo: {
-        alt: "loliot Logo",
-        src: "img/logo.svg",
-      },
-      links: [
+    title: "loliot",
+    tagline: "loliot",
+    url: "https://wiki.loliot.net",
+    baseUrl: "/",
+    favicon: "img/favicon.ico",
+    organizationName: "HandS",
+    projectName: "wiki.loliot.net",
+    plugins: [
+        "@docusaurus/plugin-google-analytics",
+        "@docusaurus/plugin-sitemap",
         {
-          to: "/docs/lang/cpp/libraries/cpp-stl",
-          label: "Programming",
-          position: "left",
-          items: [
-            {
-              label: "C/C++",
-              to: "/docs/lang/cpp/libraries/cpp-stl",
-            },
-            {
-              label: "Python",
-              to: "/docs/lang/python/libraries/python-logging",
-            },
-          ],
+            cacheTime: 600 * 1000, // 600 sec - cache purge period
+            changefreq: "weekly",
+            priority: 0.5,
         },
-        {
-          href: "https://github.com/hhk7734",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      copyright: `Copyright © ${new Date().getFullYear()} HandS. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        defaultDarkMode: true,
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          showLastUpdateTime: true,
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
     ],
-  ],
+    themeConfig: {
+        googleAnalytics: {
+            trackingID: "UA-82937088-4",
+            anonymizeIP: true,
+        },
+        navbar: {
+            title: "loliot",
+            logo: {
+                alt: "loliot Logo",
+                src: "img/logo.svg",
+            },
+            links: (() => {
+                const links = [];
+
+                let sidebarConfig = require("./sidebars");
+
+                _fs.readdirSync(path.join(path.dirname(__filename), "docs")).map((rootDir) => {
+                  const link = {
+                    label: rootDir,
+                    position: "left",
+                    items: []
+                  };
+
+                  _fs.readdirSync(path.join(path.dirname(__filename), "docs", rootDir)).map((subDir, subIndex) => {
+                    link.items.push({
+                      label: subDir,
+                      to: ""
+                    })
+                      if (subIndex === 0) link['to'] = `/docs/${sidebarConfig[subDir][Object.keys(sidebarConfig[subDir])[0]][0]}`
+                      link.items[subIndex]['to'] = `/docs/${sidebarConfig[subDir][Object.keys(sidebarConfig[subDir])[0]][0]}`
+                  });
+                  links.push(link)
+                })
+
+              links.push({
+                href: "https://github.com/hhk7734",
+                label: "GitHub",
+                position: "right",
+              })
+              return links
+            })(),
+        },
+        footer: {
+            style: "dark",
+            copyright: `Copyright © ${new Date().getFullYear()} HandS. Built with Docusaurus.`,
+        },
+    },
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            {
+                defaultDarkMode: true,
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    showLastUpdateTime: true,
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            },
+        ],
+    ],
 };
