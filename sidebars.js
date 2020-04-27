88657fcfdfeab12a99962a0f667613b4a4b725e9
Hyeonki Hong <hhk7734@gmail.com>
2020-04-27 18:28:03 +0900

    docs: update

--- sidebars.js
+++ sidebars.js
@@ -27,6 +27,20 @@ module.exports = {
       label: "Libraries",
       items: ["lang/python/libraries/python-logging"],
     },
+    {
+      type: "category",
+      label: "Package",
+      items: [
+        {
+          type: "category",
+          label: "Pybind11",
+          items: [
+            "lang/python/package/pybind11/python-package-pybind11",
+            "lang/python/package/pybind11/python-package-pybind11-type",
+          ],
+        },
+      ],
+    },
   ],
   flutter: [
     {
