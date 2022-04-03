a78f2fb42f278f4cb709156b2627c8719e95217b
Hyeonki Hong <hhk7734@gmail.com>
2022-04-03 13:33:17 +0900

    config.js: update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,3 @@
-const path = require("path");
-const _fs = require("fs");
 const remarkMath = require("remark-math");
 const mdxMermaid = require("mdx-mermaid");
 const rehypeKatex = require("rehype-katex");
@@ -40,11 +38,14 @@ module.exports = {
   ],
   themeConfig: {
     algolia: {
+      appId: "F91YRZWXMW",
       apiKey: "882821d106ded887254b7b5ec5690c5b",
       indexName: "liliot_wiki",
     },
+    autoCollapseSidebarCategories: true,
     colorMode: {
       defaultMode: "light",
+      respectPrefersColorScheme: true,
     },
     footer: {
       style: "dark",
@@ -60,96 +61,65 @@ module.exports = {
         src: "img/logo.svg",
       },
       items: (() => {
-        const links = [];
-
-        const sidebarConfig = require("./sidebars");
-        const labelDic = {
-          etc: "Etc",
-          circuit: "Circuit",
-          memo: "Memo",
-          project: "Project",
-          //
-          lang: "Programming",
-          cpp: "C++",
-          db: "Database",
-          design: "Design",
-          flutter: "Flutter",
-          go: "Go",
-          labview: "LabVIEW",
-          nodejs: "Node.js",
-          python: "Python",
-          //
-          linux: "Linux",
-          "debian-package": "Debian package",
-          "linux-kernel": "Linux kernel",
-          "linux-tools": "Linux tools",
-          "linux-uboot": "u-boot",
-          //
-          mcu: "MCU",
-          arduino: "Arduino",
-          avr: "AVR",
-          espressif: "Espressif",
-          infineon: "Infineon",
-          nordic: "Nordic",
-          sam: "SAM",
-          stm32: "STM32",
-          //
-          mlops: "MLOps",
-          nn: "NN",
-          basics: "Basics",
-          biochemistry: "BioChemistry",
+        links = {
+          Programming: {
+            Design: "design",
+            "C++": "cpp",
+            Go: "go",
+            Python: "python",
+            Database: "db",
+            "Node.js": "nodejs",
+            Flutter: "flutter",
+            LabVIEW: "labview",
+          },
+          MLOps: {
+            MLOps: "mlops",
+            NueralNetwork: "nn",
+          },
+          Linux: {
+            "Debian package": "debian-package",
+            "Linux kernel": "linux-kernel",
+            "Linux tools": "linux-tools",
+            "u-boot": "linux-uboot",
+          },
+          MCU: {
+            STM32: "stm32",
+            AVR: "avr",
+            Arduino: "arduino",
+            Espressif: "espressif",
+            SAM: "sam",
+            Infineon: "infineon",
+            Nordic: "nordic",
+          },
+          Etc: {
+            BioChemistry: "biochemistry",
+            Circuit: "circuit",
+            Memo: "memo",
+            Project: "project",
+          },
         };
 
-        _fs
-          .readdirSync(path.join(path.dirname(__filename), "docs"))
-          .map((rootDir) => {
-            // rootDir => /docs/lang, /docs/mcu, ...
-            const link = {
-              label: labelDic[rootDir],
+        return Object.entries(links)
+          .map(([key, categories]) => {
+            return {
+              type: "dropdown",
+              label: key,
               position: "left",
-              items: [],
+              items: Object.entries(categories).map(([label, to]) => {
+                return {
+                  type: "docSidebar",
+                  label: label,
+                  sidebarId: to,
+                };
+              }),
             };
-
-            _fs
-              .readdirSync(path.join(path.dirname(__filename), "docs", rootDir))
-              .map((subDir, subIndex) => {
-                // subDir => /docs/lang/cpp, /docs/mcu/infineon, ...
-                link.items.push({
-                  label: labelDic[subDir],
-                  to: "",
-                });
-
-                // sub link
-                let linkPath;
-                let item = sidebarConfig[subDir][0];
-                while (typeof item === "object") {
-                  item = item["items"][0];
-                }
-
-                linkPath = `/docs/${item}`;
-
-                link.items[subIndex]["to"] = linkPath;
-
-                // root link == first sub link
-                if (subIndex === 0) link["to"] = linkPath;
-              });
-            links.push(link);
+          })
+          .concat({
+            href: "https://github.com/hhk7734/wiki.loliot.net",
+            position: "right",
+            className: "header-github-link",
+            "aria-label": "GitHub",
           });
-
-        // right
-        // links.push({
-        //   type: "localeDropdown",
-        //   position: "right",
-        // });
-
-        links.push({
-          href: "https://github.com/hhk7734/wiki.loliot.net",
-          position: "right",
-          className: "header-github-link",
-          "aria-label": "GitHub",
-        });
-
-        return links;
       })(),
     },
     prism: {
