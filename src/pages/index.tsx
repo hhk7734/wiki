d5eea9437332fdd7d2873561564677afce3d2dd2
Hyeonki Hong <hhk7734@gmail.com>
2024-01-01 23:43:51 +0900

    docs: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import styled from "styled-components";
-import sidebarList from "../../sidebars.js";
-import * as d3 from "d3";
-import type {
-	SidebarItem,
-	SidebarItemDoc,
-	SidebarItemLink,
-	SidebarItemCategory,
-} from "@docusaurus/plugin-content-docs/src/sidebars/types.js";
+import { RadialTree } from "./radial-tree";
 
 function Home() {
 	const context = useDocusaurusContext();
@@ -22,7 +15,13 @@ function Home() {
 			description="Description will go into a meta tag in <head />"
 		>
 			<main>
-				<BrowserOnly>{() => <Tree />}</BrowserOnly>
+				<BrowserOnly>
+					{() => (
+						<Container>
+							<RadialTree />
+						</Container>
+					)}
+				</BrowserOnly>
 			</main>
 		</Layout>
 	);
@@ -30,146 +29,8 @@ function Home() {
 
 export default Home;
 
-interface TreeData {
-	name: string;
-	children?: TreeData[];
-}
-
-function itemToTreeData(item: string | SidebarItem): TreeData {
-	if (typeof item === "string") {
-		return docToTreeData(item);
-	}
-	switch (item.type) {
-		case "doc":
-			return docToTreeData(item);
-		case "link":
-			return linkToTreeData(item);
-		case "category":
-			return categoryToTreeData(item);
-		default:
-			throw new Error("Invalid item type");
-	}
-}
-
-function docToTreeData(doc: string | SidebarItemDoc): TreeData {
-	if (typeof doc === "string") {
-		doc = doc.split("/").pop();
-		const prefixList = ["arduino-", "cpp-", "flutter-", "llvm-", "python-", "sam-", "stm32-"];
-		for (const prefix of prefixList) {
-			if (doc.startsWith(prefix)) {
-				doc = doc.slice(prefix.length);
-			}
-		}
-		return { name: doc };
-	}
-	return { name: doc.label ?? doc.id };
-}
-
-function linkToTreeData(link: SidebarItemLink): TreeData {
-	return { name: link.label };
-}
-
-function categoryToTreeData(category: SidebarItemCategory): TreeData {
-	return {
-		name: category.label,
-		children: category.items.map((item) => {
-			return itemToTreeData(item);
-		}),
-	};
-}
-
-const Tree = () => {
-	const svgRef = useRef(null);
-
-	useEffect(() => {
-		const svg = d3.select(svgRef.current);
-		const width = 928;
-		const height = width;
-		const cx = width * 0.5; // adjust as needed to fit
-		const cy = height * 0.5; // adjust as needed to fit
-		const radius = Math.min(width, height) / 2 - 30;
-
-		// Create a radial tree layout. The layout’s first dimension (x)
-		// is the angle, while the second (y) is the radius.
-		const tree = d3
-			.tree()
-			.size([2 * Math.PI, radius])
-			.separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
-
-		// convert sidebarList to data
-		// sidebar is object with key: value pairs
-		// key is the name of the category
-		// value is an array of objects
-
-		const sidebarData = Object.entries(sidebarList).map(([key, value]) => {
-			return { name: key, children: value.map((item) => itemToTreeData(item)) };
-		});
-
-		const root = tree(
-			d3
-				.hierarchy({ name: "loliot", children: sidebarData })
-				.sort((a, b) => d3.ascending(a.data.name, b.data.name)),
-		);
-
-		svg
-			.attr("width", width)
-			.attr("height", height)
-			.attr("viewBox", [-cx, -cy, width, height])
-			.attr("style", "width: 100%; height: 100%; font: 10px sans-serif; font-color: black;");
-		svg
-			.append("g")
-			.attr("fill", "none")
-			.attr("stroke", "#000000")
-			.attr("stroke-opacity", 0.4)
-			.attr("stroke-width", 2)
-			.selectAll()
-			.data(root.links())
-			.join("path")
-			.attr(
-				"d",
-				d3
-					.linkRadial()
-					.angle((d) => d.x)
-					.radius((d) => d.y),
-			);
-		svg
-			.append("g")
-			.selectAll()
-			.data(root.descendants())
-			.join("circle")
-			.attr("transform", (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`)
-			.attr("fill", (d) => (d.children ? "#ff0000" : "#0000ff"))
-			.attr("r", 2.5);
-		svg
-			.append("g")
-			.attr("stroke-linejoin", "round")
-			.attr("stroke-width", 3)
-			.selectAll()
-			.data(root.descendants())
-			.join("text")
-			.attr(
-				"transform",
-				(d) =>
-					`rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0) rotate(${
-						d.x >= Math.PI ? 180 : 0
-					})`,
-			)
-			.attr("dy", "0.31em")
-			.attr("x", (d) => (d.x < Math.PI === !d.children ? 6 : -6))
-			.attr("text-anchor", (d) => (d.x < Math.PI === !d.children ? "start" : "end"))
-			.attr("paint-order", "stroke")
-			.attr("stroke", "white")
-			.attr("fill", "currentColor")
-			.text((d) => d.data.name);
-	}, []);
-
-	return <RadialTree ref={svgRef} />;
-};
-
-const RadialTree = styled.svg`
-	color: black;
-	position: relative;
-	left: 50%;
-	transform: translate(-50%, 0%);
-	background-color: #ffffff;
+const Container = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-color: white;
 `;
